e0ea799b74958bc040dbfce7543370a911036faf
rusty407 <sandroamiridze93@gmail.com>
2025-10-14 20:04:02 -0400

    first commit :)

---
+++ decrypt.py
@@ -0,0 +1,22 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+    if file == "society.py" or file == "thekey.key" or file == "decrypt.py":
+        continue
+    # append those files that u stored in for loop to Array called files
+    if os.path.isfile(file):
+        files.append(file)
+print(files)
+
+with open("thekey.key", "rb") as key:
+    secretkey = key.read()
+
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_decrypted = Fernet(secretkey).decrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_decrypted)
